cef6ffb414610b98a1490473fea8b19e50218705
seul <angel3030303@gmail.com>
2021-10-01 03:22:46 +0900

    2021.10.01

--- baekjoon/5397.cpp
+++ baekjoon/5397.cpp
@@ -1,114 +1,48 @@
 #include<iostream>
+#include<list>
 using namespace std;
-
-class Node{
-public:
-	char data;
-	Node* next;
-};
-
-class List {
-private:
-	Node* head;
-	Node* tail;
-	Node* crnt;
-public:
-	List();
-	void insert(char);
-	void forward();
-	void backward();
-	void print();
-	void delete_node();
-};
-
-List::List()
-{
-	head = tail = crnt = NULL;
-}
-
-void List::insert(char word)
-{
-	Node* new_node = new Node;
-	new_node->data = word;
-	new_node->next = NULL;
-	if (crnt == NULL)
-		head = crnt = new_node;
-	else if (crnt == tail)
-		tail = crnt = new_node;
-	else 
-		crnt->next = new_node;
-}
-
-void List::delete_node()
-{
-	if (head != NULL) {
-		if (crnt == head)
-		{
-			Node* ptr = head->next;
-			free(head);
-			head = crnt = ptr;
-		}
-		else
-		{
-			Node* ptr = head;
-			while (ptr->next != crnt)
-				ptr = ptr->next;
-			ptr->next = crnt->next;
-			free(crnt);
-			crnt = ptr;
-		}
-	}
-}
-
-void List::forward()
-{
-	if (crnt != NULL && crnt != head)
-	{
-		Node* ptr = head;
-		while (crnt != ptr->next)
-			ptr = ptr->next;
-		crnt = ptr;
-	}
-	else if (crnt == head)
-	{
-		crnt = NULL;
-	}
-}
-
-void List::backward() 
-{
-	if (crnt != NULL && crnt != tail)
-		crnt = crnt->next;
-}
 
 int main()
 {
-	List key_logger;
 	int T=0;//테스트 케이스
 	cin >> T;
 	for (int i = 0; i < T; i++)
 	{
-		for (int j = 0; j < 1000000; j++)
+		list<char> word; //list 선언
+		string word1;
+		cin >> word1;
+		auto crnt = word.end(); //auto -> 자동으로 자료형 할당 / 커서위치 crnt 선언후 word의 뒷부분 대입
+		for (int i = 0; i < word1.length(); i++)
 		{
-			char word;
-			cin >> word;
-			switch (word)
+			if (word1[i] == '<')
 			{
-			case '<':
-				key_logger.forward();
-			case'>':
-				key_logger.backward();
-			case'-':
-				key_logger.delete_node();
-			case' ':
-				break;
-			default:
-				key_logger.insert(word);
+				if (crnt != word.begin()) // <을 입력 받고 커서가 시작 점이 아니어야 커서가 앞으로 움직임
+					crnt--;
+			}
+			else if (word1[i] == '>')
+			{
+				if (crnt != word.end()) // >을 입력 받고 커서가 끝나는 점이 아니어야 커서가 뒤로 움직임
+					crnt++;
+			}
+			else if (word1[i] == '-')
+			{
+				if (crnt != word.begin()) // -을 입력 받고 커서가 시작 점이 아니어야 문자가 지워짐
+				{
+					crnt--;
+					crnt=word.erase(crnt);
+				}
+			}
+			else
+			{
+				word.insert(crnt, word1[i]); // >,<,- 을 제외한 모든 경우 커서 뒤로 단어 삽입
+			}
 		}
-
+		for (char i : word) //word 출력
+		{
+			cout << i;
 		}
+		cout << '\n';
 	}
-
 
 	return 0;
 }
