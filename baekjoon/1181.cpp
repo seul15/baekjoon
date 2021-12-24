81efaff58aaddf6becc730d24363bc16b25f02c1
seul <angel3030303@gmail.com>
2021-12-25 04:01:51 +0900

    2021.12.25

--- baekjoon/1181.cpp
+++ baekjoon/1181.cpp
@@ -1,23 +1,46 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-int main() {
-	int N;
-	cin >> N;
-	char word[50][20000] = {};
-	for (int i = 0; i < N; i++)
-		cin >> word[50][i];
+bool cmp(const string &word1,const string &word2) // 내림차순
+{
+	if (word1.size() == word2.size())
+	{
+		return word1 < word2; //(사전순으로 비교) word2가 클 경우 true word1이 클 경우 false
+	}
+	return word1.size() < word2.size(); // word2가 클 경우 true word1이 클 경우 false
+}
 
-	sort(word[50], word[50] + N);
+int main() {
+	int N;//입력 받는 단어 개수
+	cin >> N;
+	vector<string>word;
+	string w;
+	for (int i = 0; i < N; i++)
+	{	
+		cin >> w;
+		word.push_back(w);
+	}
+
+	sort(word.begin(), word.end(),cmp);
 
 	for (int i = 0; i < N; i++)
 	{
-		if (word[50][i]== word[50][i+1])
-			continue;
-		else
-			cout << word[50][i] << '\n';
+		if (i < N - 1 && word[i].size()==word[i+1].size())//다음 단어와 길이가 같은 경우
+		{
+			for (int j = 0; j < word[i].size(); j++)
+			{
+				if (word[i][j] == word[i + 1][j]) continue;// 단어가 같을 경우 스킵
+				else
+				{
+					cout << word[i] << '\n'; //아닐 경우 
+					break;
+				}
+			}
+		}
+		else cout << word[i] <<'\n';
 	}
-
+	
 	return 0;
 }
